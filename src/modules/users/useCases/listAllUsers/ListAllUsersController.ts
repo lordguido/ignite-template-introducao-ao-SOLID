8c84c2f966ce2df619f44cc3bbe6e89addc7e04f
Rodrigo Dematei de Freitas <rodfrei@gmail.com>
2021-03-21 11:52:07 -0300

    Test OK

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,16 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+    if (Array.isArray(user_id)) {
+      throw new Error("user_id need be a string");
+    }
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id });
+      return response.json(users);
+    } catch (error) {
+      return response.status(400).json({ error });
+    }
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,18 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user.admin) {
+      throw new Error("You are not an admin");
+    }
+
+    if (!user) {
+      throw new Error("User not found!");
+    }
+
+    const users = this.usersRepository.list();
+    return users;
   }
 }
 
